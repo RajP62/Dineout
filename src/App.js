4a808121c04d907d4e8cde3b403f542a82fb715c
Shivam Sharma <shivampandit635@gmail.com>
2022-01-21 10:18:49 +0530

    dineout details

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import "./App.css";
+import Routes from "./routes/Router";
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <div className="App">
+      <Routes />
+    </div>
   );
 }
 
---
+++ src/routes/Router.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {  Route } from "react-router-dom";
+import {Tabledisplaydata} from "../components/Booktable"
+
+const Routes = () => {
+  return (
+    <>
+       
+
+          <Tabledisplaydata />
+     
+    </>
+  );
+};
+
+export default Routes;
