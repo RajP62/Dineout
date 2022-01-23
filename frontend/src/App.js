cf1cb00831bce72c5a132c40e6a593ebe2757f8e
Arman <armanrajput992@gmail.com>
2022-01-23 08:19:58 +0530

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,36 @@
-import RestraurantDetail from "./Components/RestaurentDetails/RestraurantDetail";
 
-function App() {
+import './App.css';
+
+
+
+import Footer from "./Components/footer"
+// import Navbar from "./Components/navbar"
+import {Signup} from "./Components/signup"
+import { Signin } from './Components/signin';
+import TimeSlots from "./Components/Timeslot";
+export default function App() {
   return (
-    <RestraurantDetail/>
+    <div className="App">
+     
+hello
+ <div className="scroll">
+ 
+ <TimeSlots/>
+    
+   </div>
+   
+      
+   <div>
+     <Signup/>
+     <Signin/>
+    </div>
+    
+   
+
+    
+    </div>
+
+
+   
   );
 }
-
-export default App;
---
+++ frontend/src/Components/Counter.jsx
@@ -0,0 +1,7 @@
+function Counter ({num}){
+    return(
+      <h1> count is : {num}</h1>
+    )
+  }
+  
+  export default Counter
