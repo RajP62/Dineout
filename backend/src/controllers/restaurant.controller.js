da0cd91cbaa8bfa510c3eafce74bb531a01b7658
RajP62 <rajesh4151999@gmail.com>
2022-10-07 21:56:40 +0530

    sorting done

--- backend/src/controllers/restaurant.controller.js
+++ backend/src/controllers/restaurant.controller.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const Restaurants = require("../models/restaurant.model");
 const authenticate = require("../middlewares/authenticate");
 const authorise = require("../middlewares/authorization");
-const quickfilterModel = require("../models/quickfilter.model");
 
 router.get("", authenticate, authorise(['user', 'admin']), async(req,res)=>{
     try{
@@ -14,7 +13,7 @@ router.get("", authenticate, authorise(['user', 'admin']), async(req,res)=>{
         let tags = req.query?.tags || "";
         let features = req.query?.features || "";
         let dineout_passport = req.query.dineout_passport || "";
-        // let _sort = req.query?._sort || "";
+        let sort = req.query?.sort || "";
         let facilities = req.query?.facilities || "";
         let bestSelling = req.query?.bestselling || "";
         const skip = (page-1) * limit;
@@ -25,13 +24,14 @@ router.get("", authenticate, authorise(['user', 'admin']), async(req,res)=>{
         dineout_passport = dineout_passport? dineout_passport.split("&") : [];
         facilities = facilities? facilities.split("&") : [];
         bestSelling = bestSelling? bestSelling.split("&") : [];
+        sort = sort? sort==='price_asc'? {'avgcost' : 1} : sort==='price_desc'? {'avgcost': -1} : {sort : 1} : {sort : 1};
 
 
-        
-        let data = await Restaurants.aggregate([{ $lookup: { from: "cuisines", localField: "cuisine", foreignField: "ID", as: "about.cuisine" } }, { $lookup: { from: "types", localField: "type", foreignField: "ID", as: "about.type" } }, { $lookup: { from: "quickfilters", localField: "quickFilters", foreignField: "ID", as: "about.quickFilters" } }, { $lookup: { from: "facilities", localField: "facilities", foreignField: "ID", as: "about.facilities" } }, { $unset: ["about.facilities._id", "about.quickFilters._id", "about.type._id", "about.cuisine._id"] }, { $project: { "_id": 1, "imagePrimary": 1, "altImages": 1, "title": 1, "state": 1, "district": 1, "place": 1, "featured": 1, "contact": 1, "fssai": 1, "avgcost": 1, "about.cuisine": "$about.cuisine.name", "about.type": "$about.type.name", "about.quickFilters": "$about.quickFilters.name", "about.facilities": "$about.facilities.facilityName", "about.bestselling":1}}, {$match:{$and: [{$expr:{$setIsSubset:[bestSelling, "$about.bestselling"]}}, {$expr:{$setIsSubset:[facilities, "$about.facilities"]}}, {$expr:{$setIsSubset:[quick_filters, "$about.quickFilters"]}}, {$expr:{$setIsSubset:[['Andaz Delhi', '5 Star'], "$about.type"]}}, {$expr:{$setIsSubset:[cuisines, "$about.cuisine"]}}]}}, {$project: {"about":0}}]).skip(skip).limit(limit);
+        let data = await Restaurants.aggregate([{ $lookup: { from: "cuisines", localField: "cuisine", foreignField: "ID", as: "about.cuisine" } }, { $lookup: { from: "types", localField: "type", foreignField: "ID", as: "about.type" } }, { $lookup: { from: "quickfilters", localField: "quickFilters", foreignField: "ID", as: "about.quickFilters" } }, { $lookup: { from: "facilities", localField: "facilities", foreignField: "ID", as: "about.facilities" } }, { $unset: ["about.facilities._id", "about.quickFilters._id", "about.type._id", "about.cuisine._id"] }, { $project: { "_id": 1, "imagePrimary": 1, "altImages": 1, "title": 1, "state": 1, "district": 1, "place": 1, "featured": 1, "contact": 1, "fssai": 1, "avgcost": 1, "about.cuisine": "$about.cuisine.name", "about.type": "$about.type.name", "about.quickFilters": "$about.quickFilters.name", "about.facilities": "$about.facilities.facilityName", "about.bestselling":1}}, {$match:{$and: [{$expr:{$setIsSubset:[bestSelling, "$about.bestselling"]}}, {$expr:{$setIsSubset:[facilities, "$about.facilities"]}}, {$expr:{$setIsSubset:[quick_filters, "$about.quickFilters"]}}, {$expr:{$setIsSubset:[['Andaz Delhi', '5 Star'], "$about.type"]}}, {$expr:{$setIsSubset:[cuisines, "$about.cuisine"]}}]}}, {$sort : sort}]).skip(skip).limit(limit);
         // without sorting 
+        const totalPages = Math.ceil(data/limit);
 
-        return res.status(200).json({data});
+        return res.status(200).json({data, totalPages});
     }
     catch(e){
         return res.status(500).json({status:"failed",message:e.message});
