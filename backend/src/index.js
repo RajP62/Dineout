19848e5fa73f0c116582162663a0792d295ac157
DeveloperRajesh123 <rajesh.paul@collekto.in>
2023-07-17 22:26:19 +0530

    next

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,6 +11,7 @@ app.use(function(req, res, next){
   res.setHeader("Access-Control-Allow-Methods", "*");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Content-Type", "application/json;charset=UTF-8")
+  next();
 });
 // var corsOptions = {
 //     origin: 'https://singular-wisp-16a48c.netlify.app/',
