5452e6bcaeab96061c79da8cd80c7491eea03f59
DeveloperRajesh123 <rajesh.paul@collekto.in>
2023-07-17 22:10:18 +0530

    access control-retry

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,17 +5,19 @@ const fileParser = require("express-fileupload");
 app.use(express.json());
 app.use(cookieParser());
 app.use(fileParser());
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'https://dineout-clone-two.vercel.app/');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
 
+  // Pass to next layer of middleware
+  next();
+});
 // var corsOptions = {
 //     origin: 'https://singular-wisp-16a48c.netlify.app/',
 //     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 // }
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://dineout-clone-two.vercel.app/');
-    res.header("Access-Control-Allow-Private-Network", true);
-    res.header("InsecurePrivateNetworkRequestsAllowed", true);
-    next();
-  });
   
   const razorpayCont = require("./controllers/razorpay.controller");
 const restaurantCont = require("./controllers/restaurant.controller");
